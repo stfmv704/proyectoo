6999e3afb5a4911160151058e6e03f620014177c
stfmv704 <163433832+stfmv704@users.noreply.github.com>
2024-05-10 17:09:45 -0600

    Add files via upload

---
+++ Proyecto_P2-mainN/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace persona_herencia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void guardar_Click(object sender, EventArgs e)
+        {
+            //esta clase esta afuera de los if por que todos tienen un nombre, fecha de nacimiento  y edad, estos son para todas las clases
+            Persona persona = new Persona();
+            persona.Nombre = textBox1.Text;
+            persona.FeNac = textBox2.Text;
+            persona.Edad = textBox3.Text;
+            // esto se hace de tarea todos los valores
+            // deben de almacenarse en su
+            // caja de memoria correspondiente
+            // usando la clase hija que corresponda
+            if (radioButton1.Checked) //checa si el boton es seleccionado para utilizar la clase alumno
+            {
+                Alumnos fani = new Alumnos();
+                fani.Matricula = textBox4.Text;//Guarda lo que esta en el textBox4 en la clase alejandra.Matricula
+                fani.Carrera = textBox5.Text;//Guarda lo que esta en el textBox5 en la clase alejandra.Carrera
+            }
+            if (radioButton2.Checked)//checa si el boton es seleccionado para utilizar la clase Empleado
+            {
+                Empleado empleado = new Empleado(); //nueva clase empleado
+                empleado.Dni = textBox4.Text;  //Guarda lo que esta en el textBox4 en la clase empleado.Dni
+                empleado.Puesto = textBox5.Text;//Guarda lo que esta en el textBox5 en la clase empleado.Puesto
+                empleado.Sueldo = textBox6.Text;//Guarda lo que esta en el textBox6 en la clase empleado.Sueldo
+            }
+            if (radioButton3.Checked)//checa si el boton es seleccionado para utilizar la clase Docente
+            {
+                Docente docente = new Docente();
+                docente.Dni = textBox4.Text;// Guarda lo que esta en el textBox4 en la case docente.Dni
+                docente.Puesto = textBox5.Text;// Guarda lo que esta en el textBox4 en la case docente.Puesto
+                docente.Sueldo = textBox6.Text;// Guarda lo que esta en el textBox4 en la case docente.Sueldo
+            }
+        }
+
+        private void limpiar_Click(object sender, EventArgs e)
+        {
+            // se hace de tarea
+            // debe de borrar las cajas de texto
+            textBox1.Clear(); //borra lo de textbox1
+            textBox2.Clear();//borra lo de textbox2
+            textBox3.Clear();//borra lo de textbox3
+            textBox4.Clear();//borra lo de textbox4
+            textBox5.Clear();//borra lo de textbox5
+            textBox6.Clear();//borra lo de textbox6
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            // se hace de tarea
+            // debe de terminar la ejecucion de la interfaz
+            Application.Exit(); //sale de aplicacion
+        }
+    }
+}
---
+++ Proyecto_P2-mainN/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace persona_herencia
+{
+    class Persona //clase padre
+    {
+        //variables
+        protected string nombre;
+        protected string fecha;
+        protected string edad;
+        //atributos
+        //metodos
+        public string Nombre { get; set; }
+        public string FeNac { get; set; }
+        public string Edad { get; set; }
+        //colocar todos los comentarios necesarios para explicar
+        //el funcinamiento de su código
+        //aqui va el constructor para cargar datos en las cajas de memoria
+
+        //nombre, edad, fecha de nacimiento(es un tipo cadena!!!!! string)
+    }
+    class Alumnos : Persona
+    {
+        // Atributos de la clase 'Alumno'
+        protected string matricula;
+        protected string carrera;
+        // Constructor de la clase 'Alumno'
+        // Métodos de la clase 'Alumno'
+        public string Matricula { get; set; }
+        public string Carrera { get; set; }
+        //colocar todos los comentarios necesarios para explicar
+        //el funcinamiento de su código
+    }
+    class Empleado : Persona //clase hija
+    {
+        // Atributos de la clase 'Alumno'
+        protected string Antonio;
+        protected string puesto;
+        protected string sueldo;
+        // Constructor de la clase 'Alumno'
+        // Métodos de la clase 'Alumno'
+        public string Dni { get; set; }
+        public string Puesto { get; set; }
+        public string Sueldo { get; set; }
+        //colocar todos los comentarios necesarios para explicar
+        //el funcinamiento de su código
+    }
+
+    class Docente : Persona //clase Hija
+    {
+        // Atributos de la clase 'Alumno'
+        protected string Oscar;
+        protected string puesto;
+        protected string sueldo;
+        // Constructor de la clase 'Alumno'
+        // Métodos de la clase 'Alumno'
+        public string Dni { get; set; }
+        public string Puesto { get; set; }
+        public string Sueldo { get; set; }
+        //colocar todos los comentarios necesarios para explicar
+        //el funcinamiento de su código
+    }
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
+
